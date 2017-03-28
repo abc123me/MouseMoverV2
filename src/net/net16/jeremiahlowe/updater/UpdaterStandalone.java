37c837c21a5d6cb3468cda3c342598e9dbf5533d
abc123me <jeremiah_abc123me@hotmail.com>
2017-03-27 21:57:17 -0400

    Updated updater (UPDATE_CEPTION)

--- src/net/net16/jeremiahlowe/updater/UpdaterStandalone.java
+++ src/net/net16/jeremiahlowe/updater/UpdaterStandalone.java
@@ -6,7 +6,7 @@ public class UpdaterStandalone {
 	public static final String VERSION_FILE_URL = "https://raw.githubusercontent.com/abc123me/MouseMoverV2/master/Updater/version";
 	public static final String VERSION_FILE_NAME = "version";
 	public static void main(String[] args) throws Throwable{
-		System.out.println(Updater.needsUpdate(new File("updater/version"), "1.1"));
+		System.out.println(Updater.needsUpdate(new File("updater/version"), "1.3"));
 	}
 	
 }
